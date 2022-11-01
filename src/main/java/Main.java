ca9972783c4f5d0f1e87c448629b8149e26853c4
Troy2797B <112650105+Troy2797B@users.noreply.github.com>
2022-11-01 16:02:26 -0400

    got the input to go, just have to figure out how to manipulate it

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,44 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        /*Scanner scanObj = new Scanner(System.in);
+        System.out.println("Enter a number here!");
+        int n = scanObj.nextInt();*/
+        //addNumber();
+        //getInput();
+        Scanner scanObj = new Scanner(System.in);
+        System.out.println("Enter a number here!");
+        int n = scanObj.nextInt();
 
     }
-    
-    int sumOfNumbers(int n) {
-        return 0;
-    }
+
+    //first find the input using Scanner class
+   /* public static int getInput() {
+        Scanner scanObj = new Scanner(System.in);
+        System.out.println("Enter a number here!");
+        int n = scanObj.nextInt();
+        return n;
+    }*/
+
+}
+
+
+    /* factor = 1;
+        for (int i = 2; i < getInput(); i++){
+            int sum = 0;
+            int sumOfNumbers = sum + i;
+        } return sumOfNumbers;*/
     
     // for Extra Credit
-    int gaussianSumOfNumbers(int n) {
+    /*int gaussianSumOfNumbers(int n) {
         return 0;
-    }
+    }*/
    
     // for Extra extra credit - compare the methods and show which one is faster
     
-}
+
