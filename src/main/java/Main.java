26887a1a1f2040907233626077aae62a57b9848b
Troy2797B <112650105+Troy2797B@users.noreply.github.com>
2022-11-02 09:28:00 -0400

    finally done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,15 +6,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        /*Scanner scanObj = new Scanner(System.in);
-        System.out.println("Enter a number here!");
-        int n = scanObj.nextInt();*/
         //addNumber();
         //getInput();
         Scanner scanObj = new Scanner(System.in);
         System.out.println("Enter a number here!");
         int n = scanObj.nextInt();
 
+        int fact = 1;
+        for (int i =2; i <= n; i++){
+            fact = fact + i;
+            System.out.println(fact);
+        }
     }
 
     //first find the input using Scanner class
